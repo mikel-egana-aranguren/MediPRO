663ad4ab320fcacca1ea5595ffedb3783c060444
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2016-12-08 13:14:20 +0100

    First working script: loads ontology, modifies it, saves it

--- eclipse_workspace/MediProAnatomicalPartonomy/src/medipro/core.clj
+++ eclipse_workspace/MediProAnatomicalPartonomy/src/medipro/core.clj
@@ -1,6 +1,7 @@
 (ns medipro.core
   (:use [tawny.owl] 
-        [tawny.lookup])
+        [tawny.lookup]
+        [tawny.profile])
   (:require [tawny
              [read]
              [polyglot]
@@ -11,83 +12,84 @@
   
   )
 
-(defontology medipro
-  :iri "http://mikeleganaaranguren.com/medipro"
-  :prefix "medipro:"
-  :comment "Testing medipro manipulation without loading an ontology"
-  :versioninfo "Unreleased Version")
-
-(as-inverse
- (defoproperty hasAnatomicalPart
-   :characteristics :transitive)
- (defoproperty isAnatomicalPartf
-   :characteristics :transitive
+(defn get-medipro-ontology []
+  (tawny.owl/remove-ontology-maybe
+   (OWLOntologyID. 
+     (IRI/create "http://mikeleganaaranguren.com/MediPRO/AnatomicalPartonomy")
+                   )
    )
- )
-
-(defn generate-whole-parts [ & wholeparts]
-  (doseq
-      [[whole & parts] wholeparts]
-    (owl-class
-     whole
-     :super (some-only hasAnatomicalPart parts))))
-
-(declare-classes 
-  stomach pylorus body-of-stomach fundus antrum
-  eh bizkaia gipuzkoa araba nafarroa zuberoa behenafarroa lapurdi
+  (.loadOntologyFromOntologyDocument
+   (tawny.owl/owl-ontology-manager)
+   (IRI/create 
+     (clojure.java.io/resource "AnatomicalPartonomy.owl")
+     )
+   )
   )
 
-(generate-whole-parts 
-  [stomach pylorus body-of-stomach fundus antrum]
-  [eh bizkaia gipuzkoa araba nafarroa zuberoa behenafarroa lapurdi]
+(def medipro 
+  (get-medipro-ontology)
   )
 
+(println 
+  (inprofile? medipro profile-owl2el)
+  )
 
-(save-ontology "ontology.owl" :owl)
+(defclass mikel
+  :ontology medipro
+  :label "mikel")
+
+(save-ontology medipro "AnatomicalPartonomy2.owl" :owl)
 
 
 
 
 
+;;;; Make loaded ontology the default (not recommended)
 
-
-
-;(defn get-go-ontology []
-;  (tawny.owl/remove-ontology-maybe
-;   (OWLOntologyID. 
-;     (IRI/create "http://purl.obolibrary.org/obo/go.owl")
-;                   )
-;   )
-;  (.loadOntologyFromOntologyDocument
-;   (tawny.owl/owl-ontology-manager)
-;   (IRI/create 
-;     (clojure.java.io/resource "go-snippet.owl")
-;     )
-;   )
+;(ontology-to-namespace 
+;  (get-medipro-ontology)
 ;  )
-
-;(println (get-current-ontology))
-
-;(println (get-go-ontology))
-
-
-
-
-
-
-;(with-ontology
-;  get-go-ontology)
 ;
-;(println (subclasses "http://purl.obolibrary.org/obo/GO_0000002"))
-       
-;(defontology "http://purl.obolibrary.org/obo/go.owl")
+;(declare-classes 
+;  eh bizkaia gipuzkoa araba nafarroa zuberoa behenafarroa lapurdi
+;  bilbo donosti berlin
+;  )
+;
+;(save-ontology "AnatomicalPartonomy2.owl" :owl)
 
+
+
+;;;; Create ontology from scratch
+
+;(defontology medipro
+;  :iri "http://mikeleganaaranguren.com/medipro"
+;  :prefix "medipro:"
+;  :comment "Testing medipro manipulation without loading an ontology"
+;  :versioninfo "Unreleased Version")
+;
+;(as-inverse
+; (defoproperty hasAnatomicalPart
+;   :characteristics :transitive)
+; (defoproperty isAnatomicalPartf
+;   :characteristics :transitive
+;   )
+; )
+;
+;(defn generate-whole-parts [ & wholeparts]
+;  (doseq
+;      [[whole & parts] wholeparts]
+;    (owl-class
+;     whole
+;     :super (some-only hasAnatomicalPart parts))))
+;
+;(declare-classes 
+;  stomach pylorus body-of-stomach fundus antrum
+;  eh bizkaia gipuzkoa araba nafarroa zuberoa behenafarroa lapurdi
+;  )
+;
+;(generate-whole-parts 
+;  [stomach pylorus body-of-stomach fundus antrum]
+;  [eh bizkaia gipuzkoa araba nafarroa zuberoa behenafarroa lapurdi]
+;  )
+;
 ;(save-ontology "ontology.owl" :owl)
-  
-;(defn foo
-;  "I don't do a whole lot."
-;  [x]
-;  (println x "Hello, World!"))
-;
-;
-;(foo "xxx")
