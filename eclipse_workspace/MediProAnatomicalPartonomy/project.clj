c930865a0161fc16ee48137eaaa502e149060147
mikel-egana-aranguren <mikel.egana.aranguren@gmail.com>
2017-02-25 12:43:25 +0100

    updated to twany-owl 1.6

--- eclipse_workspace/MediProAnatomicalPartonomy/project.clj
+++ eclipse_workspace/MediProAnatomicalPartonomy/project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [org.clojure/clojure "1.6.0"]
-                 [uk.org.russet/tawny-owl "1.5.0"]
+                 [uk.org.russet/tawny-owl "1.6.0"]
                  ]
-  
   )
